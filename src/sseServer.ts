99b686b1717f093b957b76ac39aa1df07280dbf6
Yury Shkoda <shkoda.yuri@gmail.com>
2022-12-15 17:17:00 +0300

    chore(sse): added SSE server for debugging

---
+++ src/sseServer.ts
@@ -0,0 +1,39 @@
+const http = require('http')
+const os = require('os')
+
+const host = '127.0.0.1'
+const port = 8080
+
+let dataSource = 0
+const updateDataSource = () => {
+  const delta = Math.random()
+
+  dataSource += delta
+}
+
+const requestListener = function (req: any, res: any) {
+  if (req.url === '/ticker') {
+    res.statusCode = 200
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Cache-Control', 'no-cache')
+    res.setHeader('connection', 'keep-alive')
+    res.setHeader('Content-Type', 'text/event-stream')
+
+    setInterval(() => {
+      const data = JSON.stringify({ ticker: dataSource })
+
+      res.write(`id: ${new Date().toLocaleTimeString()}\ndata: ${data}\n\n`)
+    }, 1000)
+  } else {
+    res.statusCode = 404
+    res.end('resource does not exist')
+  }
+}
+
+const server = http.createServer(requestListener)
+
+server.listen(port, host, () => {
+  setInterval(() => updateDataSource(), 500)
+
+  console.log(`server running at http://${host}:${port}`)
+})
