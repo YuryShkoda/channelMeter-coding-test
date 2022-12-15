6b149a632ff0204376fa6d1696020604365df1ce
Yury Shkoda <shkoda.yuri@gmail.com>
2022-12-15 17:18:04 +0300

    feat(server): configured initial server

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+const EventSource = require('../../eventsource/lib/eventsource.js')
+
+const app = express()
+const port = 5000
+
+const server = app.listen(port, () => {
+  console.log(`🤖 Server is running at http://localhost:${port}`)
+})
+
+const es = new EventSource('http://live-test-scores.herokuapp.com/scores')
+
+es.onmessage = (event: any) => {
+  console.log(`🤖[event.data]🤖`, event.data)
+}
+
+app.get('/', (_, res) => {
+  res.send('Welcome to this awesome API :)')
+})
+
+export default server
